5a40873239589f347d732745b2a8ed3f34d2610d
Mohd Shahmi Abdul Majid <mshahmi@moneylion.com>
2016-07-20 15:34:52 +0800

    Remove vars as cronjob not able to pickup vars from source

--- filechecker.sh
+++ filechecker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
  
-configfile="/home/filecheckrename/vars/five9.cfg"
-source "$configfile"
+filepath="/home/johndoe/reports/reportinfo.md"
+ext="csv"
+reportpath="/home/johndoe/reports"
+
 file="$filepath"."$ext"
-extension="$ext"
  
 if [ -f "$file" ]
 then
-         mv -nv -- "$file" "$filepath"_"$(date -r "$file" +"%Y%m%d_%H%M").$extension"
-         chown -R five9:five9 $reportpath
+         mv -nv -- "$file" "$filepath"_"$(date -r "$file" +"%Y%m%d_%H%M").$ext"
+         chown -R www:www $reportpath
 else
          echo $file "not found."
 fi
